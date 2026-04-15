5b7d1794f6d5fcdcfec037b8bfe1f62bda986d43
PriyadarshiniPriya-23 <priya.d@wizzybox.com>
2026-04-15 10:57:39 +0530

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
