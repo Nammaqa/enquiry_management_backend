cc660605510d34af7c7add94f5c7932bb3ad7d64
PriyadarshiniPriya-23 <priya.d@wizzybox.com>
2026-05-12 12:39:29 +0530

    created payment history model and api

---
+++ src/models/billingpaymenthistory.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'BillingPaymentHistory',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      billingId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'billings',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      amountPaid: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      paymentMode: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        comment: 'e.g. UPI, CARD, CASH',
+      },
+      transaction_id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        comment: 'Transaction ID for UPI/Card payments',
+      },
+    },
+    {
+      tableName: 'billing_payment_history',
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+};
